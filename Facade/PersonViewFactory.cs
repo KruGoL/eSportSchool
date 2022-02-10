9f07d5ceb3af0f7fa1c17eaaf8d57f8858806a4c
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-02-10 20:56:01 +0200

    edited for beauty

--- Facade/PersonViewFactory.cs
+++ Facade/PersonViewFactory.cs
@@ -9,21 +9,20 @@ namespace eSportSchool.Facade
     public class PersonViewFactory
     {
         public Person Create(PersonView v) => new(new PersonData() {
-           
-                Id = v.Id,
-                DoB = v.DoB,
-                Gender = v.Gender,
-                FirstName = v.FirstName,
-                LastName = v.LastName
+            Id = v.Id,
+            DoB = v.DoB,
+            Gender = v.Gender,
+            FirstName = v.FirstName,
+            LastName = v.LastName
         });
     
         public PersonView Create(Person o)=> new() {
-                Id = o.Id,
-                DoB = o.DoB,
-                Gender = o.Gender,
-                FirstName = o.FirstName,
-                LastName = o.LastName,
-                FullName = o.ToStrnig(),
+            Id = o.Id,
+            DoB = o.DoB,
+            Gender = o.Gender,
+            FirstName = o.FirstName,
+            LastName = o.LastName,
+            FullName = o.ToStrnig(),
         };
     
     }
