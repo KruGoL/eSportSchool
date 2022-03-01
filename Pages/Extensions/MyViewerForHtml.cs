3ce625146739efa990ea359a2f14c48dd683f7a4
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-03-01 14:38:47 +0200

    SportTeamsPage,TrainersPage : Edit, Details , Delete , Index

---
+++ Pages/Extensions/MyViewerForHtml.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace eSportSchool.Pages.Extensions
+{
+    public static class MyViewerForHtml
+    {
+        public static IHtmlContent MyViewerFor<TModel, TResult>(
+            this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+        {
+            var s = HtmlStrings(h, e);
+            return new HtmlContentBuilder(s);
+        }
+
+        private static List<object> HtmlStrings<TModel, TResult>(IHtmlHelper<TModel> h,
+            Expression<Func<TModel, TResult>> e)
+        {
+            var l = new List<object>();
+
+            l.Add(new HtmlString("<dl class=\"row\">"));
+            l.Add(new HtmlString("<dt class=\"col-sm-2\">"));
+            l.Add(h.DisplayNameFor(e));
+            l.Add(new HtmlString("</dt>"));
+            l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
+            l.Add(h.DisplayFor(e));
+            l.Add(new HtmlString("</dd>"));
+            l.Add(new HtmlString("</dl>"));
+
+            return l;
+        }
+    }
+}
