69954905e4b0accc812539fb8cf4af2f51db7b0b
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-03-28 19:21:27 +0300

    AddressDataTests

---
+++ Tests/Data/Party/AddressDataTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using eSportSchool.Data.Party;
+
+namespace eSportSchool.Tests.Data.Party
+{
+    [TestClass]
+    public class AddressDataTests: SealedClassTests<AddressData>
+    {
+        [TestMethod] public void IdTest() => IsProperty<string>();
+        [TestMethod] public void StreetTest() => IsProperty<string?>();
+        [TestMethod] public void CityTest() => IsProperty<string?>();
+        [TestMethod] public void RegionTest() => IsProperty<string?>();
+        [TestMethod] public void ZipCodeTest() => IsProperty<string?>();
+        [TestMethod] public void CountryTest() => IsProperty<string?>();
+
+    }
+}
