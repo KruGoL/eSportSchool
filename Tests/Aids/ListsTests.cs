93b9e32d0d529b42bfae3d954a0b16293224fdc2
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-03-29 19:53:04 +0300

    GetAssemblyTests static Assembly a

--- Tests/Aids/ListsTests.cs
+++ Tests/Aids/ListsTests.cs
@@ -1,5 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace eSportSchool.Tests.Aids {
-    [TestClass] public class ListsTests : IsTypeTested { }
+    [TestClass] public class ListsTests : IsTypeTested {
+        [TestMethod]
+        public void GetFirstTest() => isInconclusive();
+        [TestMethod]
+        public void RemoveTest() => isInconclusive();
+        [TestMethod]
+        public void IsEmptyTest() => isInconclusive();
+        [TestMethod]
+        public void ContainsItemTest() => isInconclusive();
+    }
 }
