938f7187dc0f4b100af558be8fec36075dfa7e06
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-03-29 19:11:04 +0300

    GetAssemblyTests OfTypeTest()

--- Tests/Aids/GetAssemblyTests.cs
+++ Tests/Aids/GetAssemblyTests.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using eSportSchool.Aids;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
 
 namespace eSportSchool.Tests.Aids {
     [TestClass]
     public class GetAssemblyTests : IsTypeTested
     {
-        [TestMethod] public void ByNameTest() => isInconclusive();
-        [TestMethod] public void OfTypeTest() => isInconclusive();
+        [TestMethod] public void ByNameTest() {
+            var name = Assembly.GetCallingAssembly().GetName().Name;
+            var gName= GetAssembly.ByName(name);
+            name += ".dll";
+            areEqual(gName.ManifestModule.Name, name);
+        }
+        [TestMethod] public void OfTypeTest()
+        {
+            var a = Assembly.GetCallingAssembly();
+            var aType = GetAssembly.OfType(a);
+            areEqual(a.GetType().Assembly,aType);
+        }
         [TestMethod] public void TypesTest() => isInconclusive();
         [TestMethod] public void TypeTest() => isInconclusive();
     }
