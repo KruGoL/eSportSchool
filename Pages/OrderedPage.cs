0ad692b2cf063ea7f060396727ea2932875d1be1
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-04-19 14:11:53 +0300

    OrderedPage fix

--- Pages/OrderedPage.cs
+++ Pages/OrderedPage.cs
@@ -1,5 +1,7 @@
 ﻿using eSportSchool.Domain;
 using eSportSchool.Facade;
+using System.ComponentModel;
+using System.Reflection;
 
 namespace eSportSchool.Pages
 {
@@ -11,9 +13,35 @@ namespace eSportSchool.Pages
         protected OrderedPage(TRepo r) : base(r) { }
         public string? CurrentOrder
         {
-            get => repo.CurrentOrder;
-            set => repo.CurrentOrder = value;
+            get => fromCurrentOrder(repo.CurrentOrder);
+            set => repo.CurrentOrder = toCurrentOrder(value);
         }
-        public string? SortOrder(string propertyName) => repo.SortOrder(propertyName);
+        private string? fromCurrentOrder(string? value)
+        {
+            var isDesc = value?.Contains("_desc") ?? false;
+            var propertyName = value?.Replace("_desc", string.Empty);
+            var pi = typeof(TView).GetProperty(propertyName);
+            var displayName = getDisplayName(pi);
+            return isDesc ? displayName + "_desc" : displayName;
+        }
+        private static string? getDisplayName(PropertyInfo? pi)
+        {
+            var dn = pi?.GetCustomAttribute<DisplayNameAttribute>();
+            return dn?.DisplayName;
+        }
+        private string? toCurrentOrder(string? value)
+        {
+            var isDesc = value?.Contains("_desc") ?? false;
+            var displayName = value?.Replace("_desc", string.Empty);
+            foreach (var pi in typeof(TView).GetProperties())
+            {
+                if (!isThisDisplayName(pi, displayName)) continue;
+                return isDesc ? pi.Name + "_desc" : pi.Name;
+            }
+            return value;
+        }
+        private static bool isThisDisplayName(PropertyInfo pi, string? displayName)
+            => getDisplayName(pi) == displayName;
+        public string? SortOrder(string displayName) => repo.SortOrder(toCurrentOrder(displayName));
     }
 }
