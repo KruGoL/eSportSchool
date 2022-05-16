4f84e2a37bd76cfc608f9c1e37ea2c5620e0bbb8
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-05-16 22:03:33 +0300

    Table can show photo

--- Pages/Extensions/ShowTableHtml.cs
+++ Pages/Extensions/ShowTableHtml.cs
@@ -6,12 +6,12 @@ namespace eSportSchool.Pages.Extensions {
     public static class ShowTableHtml
     {
         public static IHtmlContent ShowTable<TModel, TView>(
-            this IHtmlHelper<TModel> h, IList<TView>? items)
+            this IHtmlHelper<TModel> h, IList<TView>? items ,string? imgPath = null)
                 where TModel : IIndexModel<TView> where TView : UniqueView {
-            var s = htmlStrings(h, items);
+            var s = htmlStrings(h, items , imgPath);
             return new HtmlContentBuilder(s);
         }
-        private static List<object> htmlStrings<TModel, TView>(IHtmlHelper<TModel> h, IList<TView>? items)
+        private static List<object> htmlStrings<TModel, TView>(IHtmlHelper<TModel> h, IList<TView>? items , string? imgPath = null)
             where TModel : IIndexModel<TView> where TView : UniqueView {
             var m = h.ViewData.Model;
             var l = new List<object> {
@@ -20,7 +20,7 @@ namespace eSportSchool.Pages.Extensions {
                 new HtmlString("<tr>")
             };
             foreach (var name in m.IndexColumns) {
-                l.Add(new HtmlString("<td>"));
+                l.Add(new HtmlString("<td>"));               
                 l.Add(h.MyTabHdr(m.DisplayName(name)));
                 l.Add(new HtmlString("</td>"));
             }
@@ -32,7 +32,17 @@ namespace eSportSchool.Pages.Extensions {
                 l.Add(new HtmlString("<tr>"));
                 foreach (var name in m.IndexColumns) {
                     l.Add(new HtmlString("<td>"));
-                    l.Add(h.Raw(m.GetValue(name, item)));
+                    var val = m.GetValue(name, item)?.ToString();
+                    if (isImg(val))
+                    {                            
+                        l.Add(new HtmlString($"<img class=\"rounded mx - auto d - block\""));
+                        l.Add(new HtmlString($"src =\"img/{imgPath}/{val}\""));
+                        l.Add(new HtmlString($"width =\"100\""));
+                        l.Add(new HtmlString($"height =\"125\""));
+                        l.Add(new HtmlString($"asp - append - version =\"true\""));
+                        l.Add(new HtmlString($"asp -append-version=\"true\" />"));
+                    }
+                    else l.Add(h.Raw(m.GetValue(name, item)));                                                                                
                     l.Add(new HtmlString("</td>"));
                 }
                 l.Add(new HtmlString("<td>"));
@@ -43,6 +53,18 @@ namespace eSportSchool.Pages.Extensions {
             l.Add(new HtmlString("</tbody>"));
             l.Add(new HtmlString("</table>"));
             return l;
+        }
+        private static bool isImg (string? str)
+        {
+            if(str?.Length > 4)
+            {
+                string s = str.Substring(str.Length - 4);
+                if (s == ".jpg" || s == ".png")
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
