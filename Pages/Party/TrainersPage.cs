9a065411b03184bc0878bdd627be6a2fe1f56c9f
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-05-17 21:17:49 +0300

    UploadPage created =>    TrainersPage : UploadPage

--- Pages/Party/TrainersPage.cs
+++ Pages/Party/TrainersPage.cs
@@ -9,15 +9,15 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace eSportSchool.Pages.Party
 {
-    public class TrainersPage : PagedPage<TrainerView, Trainer, ITrainersRepo>
+    public class TrainersPage : UploadPage<TrainerView, Trainer, ITrainersRepo>
     {
-        [BindProperty]
-        public IFormFile? Photo { get; set; }
-        IHostingEnvironment _webHostEnv;
-        public TrainersPage(ITrainersRepo r, IHostingEnvironment webHostEnv) : base(r)
+        public TrainersPage(ITrainersRepo r, IHostingEnvironment webHostEnv ) :base(r, webHostEnv)
         {
-            _webHostEnv = webHostEnv;
+            _item = "ImgPath";
+            _folderPath = "img/trainers";
+            _defaultFileName = "default.jpg";
         }
+
         protected override Trainer toObject(TrainerView? item) => new TrainerViewFactory().Create(item);
         protected override TrainerView toView(Trainer? entity) => new TrainerViewFactory().Create(entity);
         public override string[] IndexColumns { get; } = new[] {
@@ -39,41 +39,6 @@ namespace eSportSchool.Pages.Party
             var r = base.GetValue(name, v);          
             return name == nameof(TrainerView.Gender) ? GenderDescription((IsoGender)r) : r;
         }
-        public List<SportTeam?> SportTeams => toObject(Item).SportTeams;
-        protected override async Task<IActionResult> postEditAsync()
-        {
-            if (Photo != null)
-            {
-                if (Item.ImgPath != null)
-                {
-                    string filePath = Path.Combine(_webHostEnv.WebRootPath, "img/trainers", Item.ImgPath);
-                    System.IO.File.Delete(filePath);
-                }
-                Item.ImgPath = UploadFile();
-            }
-
-            return await base.postEditAsync();
-        }
-        protected override async Task<IActionResult> postCreateAsync()
-        {
-            if (Photo != null)
-            {
-                Item.ImgPath = UploadFile();
-            }
-            return await base.postCreateAsync();
-        }
-        private string UploadFile()
-        {
-            string? uniqueFileName = null;
-            if (Photo != null)
-            {
-                string uploadsFolder = Path.Combine(_webHostEnv.WebRootPath, "img/trainers");
-                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
-                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-                using (var fs = new FileStream(filePath, FileMode.Create)) { Photo.CopyTo(fs); }
-            }
-            return uniqueFileName ?? "default.jpg";
-        }
+        public List<SportTeam?> SportTeams => toObject(Item).SportTeams;        
     }
 }
---
+++ Pages/UploadPage.cs
@@ -0,0 +1,77 @@
+﻿using eSportSchool.Domain;
+using eSportSchool.Facade;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace eSportSchool.Pages
+{
+    public abstract class UploadPage<TView, TEntity, TRepo> : PagedPage<TView, TEntity, TRepo>,
+        IPageModel, IIndexModel<TView>
+        where TView : UniqueView, new()
+        where TEntity : UniqueEntity
+        where TRepo : IPagedRepo<TEntity>
+    {
+        [BindProperty]
+        public IFormFile? File { get; set; }
+        protected string _item;
+        protected string _folderPath;
+        protected string _defaultFileName; 
+        private 
+        IHostingEnvironment _webHostEnv;
+        protected UploadPage(TRepo r, IHostingEnvironment webHostEnv) : base(r){_webHostEnv = webHostEnv;}
+        protected string UploadFile(string folderPath,string defaultFileName = "Unknown file")
+        {
+            string? uniqueFileName = null;
+            if (File != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnv.WebRootPath, folderPath);
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + File.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fs = new FileStream(filePath, FileMode.Create)) { File.CopyTo(fs); }
+            }
+            return uniqueFileName ?? defaultFileName;
+        }
+        protected override async Task<IActionResult> postEditAsync()
+        {
+            if (File != null)
+            {
+                PropertyInfo p = GetPropertyFile(_item);
+                if (p != null)
+                {
+                    if (p.GetValue(Item) != null)
+                    {
+                        string filePath = Path.Combine(_webHostEnv.WebRootPath, _folderPath, p.GetValue(Item).ToString());
+                        var my = p.GetValue(Item).ToString();
+                        if (my != _defaultFileName)
+                        {
+                            System.IO.File.Delete(filePath);
+                        }
+                    }
+                    p.SetValue(Item, UploadFile(_folderPath, _defaultFileName));
+                }               
+            }
+            return await base.postEditAsync();
+        }
+        protected override async Task<IActionResult> postCreateAsync()
+        {
+            PropertyInfo p = GetPropertyFile(_item) ;
+            if (p != null) { p.SetValue(Item, UploadFile(_folderPath, _defaultFileName)); }
+            return await base.postCreateAsync();
+        }
+        private PropertyInfo? GetPropertyFile(string itemName)
+        {
+            Type t = typeof(TView);
+            PropertyInfo[] propInfos = t.GetProperties();
+            foreach (PropertyInfo p in propInfos)
+            {
+                if (p.Name == itemName) return p;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Property to create file does not exist ");
+            Console.ResetColor();
+            return null;
+        }
+    }
+}
